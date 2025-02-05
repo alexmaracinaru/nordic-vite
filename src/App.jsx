b392aa298c323a3f64ff70b1a8d76ab52e8756d1
Alex Maracinaru <63877489+alexmaracinaru@users.noreply.github.com>
2025-02-05 18:30:49 +0100

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/">
       <div className="App">
         <Navbar />
         <Routes>
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", //this is what "homepage" did in the package.json of CRA projects.
 });
